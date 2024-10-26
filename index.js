9b33bb357f6b2f2f1863a1ae2c70edc8bfa693c3
VuongHuuAn <anvuong156@gmail.com>
2024-10-27 00:59:39 +0700

    Update index.js
    dasd

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-console.log("Hello World");
+// IMPORTS FROM PACKAGES
 const express = require("express");
+const mongoose = require("mongoose");
+const adminRouter = require("./routes/admin");
+// IMPORTS FROM OTHER FILES
 const authRouter = require("./routes/auth");
-const { default: mongoose } = require("mongoose");
+const productRouter = require("./routes/product");
+const userRouter = require("./routes/user");
+
+// INIT
 const PORT = process.env.PORT || 3000;
 const app = express();
 const DB =
   "mongodb+srv://anvuong156:super156@cluster0.zgceu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const adminRouter = require("./routes/admin");
-const productRouter = require("./routes/product");
-const userRouter = require("./routes/user");
+// middleware
 app.use(express.json());
 app.use(authRouter);
 app.use(adminRouter);
 app.use(productRouter);
 app.use(userRouter);
-app.use((req, res) => {
-  res.status(404).json({ error: "Route không tồn tại" });
-});
+
+// Connections
 mongoose
   .connect(DB)
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log("Connection Successful");
   })
   .catch((e) => {
     console.log(e);
   });
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`connected at port ${PORT}`);
 });
-module.exports = app;
