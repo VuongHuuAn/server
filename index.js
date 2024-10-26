e0e4f7c59394a2af41090034394f15ef3164804f
VuongHuuAn <anvuong156@gmail.com>
2024-10-26 09:02:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 console.log("Hello World");
 const express = require("express");
-const authRouter=require("./routes/auth");
+const authRouter = require("./routes/auth");
 const { default: mongoose } = require("mongoose");
-const PORT=3000;
-const app=express();
-const DB="mongodb+srv://anvuong156:super156@cluster0.zgceu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" 
-const adminRouter = require("./routes/admin"); 
+const PORT = 3000;
+const app = express();
+const DB =
+  "mongodb+srv://anvuong156:super156@cluster0.zgceu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const adminRouter = require("./routes/admin");
 const productRouter = require("./routes/product");
 const userRouter = require("./routes/user");
 app.use(express.json());
@@ -14,13 +15,17 @@ app.use(adminRouter);
 app.use(productRouter);
 app.use(userRouter);
 app.use((req, res) => {
-    res.status(404).json({ error: "Route không tồn tại" });
+  res.status(404).json({ error: "Route không tồn tại" });
 });
-mongoose.connect(DB).then(()=>{
+mongoose
+  .connect(DB)
+  .then(() => {
     console.log("Connected to MongoDB");
-}).catch((e)=>{
+  })
+  .catch((e) => {
     console.log(e);
-}); 
-app.listen(PORT,()=>{
-    console.log(`connected at port ${PORT}`);
-})  
+  });
+app.listen(PORT, () => {
+  console.log(`connected at port ${PORT}`);
+});
+module.exports = app;
